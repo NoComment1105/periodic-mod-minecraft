6009feb7e20227d3f1d8c5c197b177e442dc2303
NoComment1105 <ryan.lillistone@gmail.com>
2021-03-02 14:13:09 +0000

    Fix Mod after i broke it ;-;

--- src/main/java/com/NoComment1105/periodic/registry/ModBlocks.java
+++ src/main/java/com/NoComment1105/periodic/registry/ModBlocks.java
@@ -8,7 +8,7 @@ import net.minecraft.block.Material;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
-;
+
 
 public class ModBlocks {
 
