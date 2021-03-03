09c539de89d0ed085d43026c88bc4bb675613ca5
NoComment1105 <ryan.lillistone@gmail.com>
2021-03-03 10:24:59 +0000

    Revert "Creating Uranium Ore"
    This reverts commit 6b6b3293faa31749204d0b1c54b4a9e3fee9a5e5.

--- src/main/java/com/NoComment1105/periodic/registry/ModItems.java
+++ src/main/java/com/NoComment1105/periodic/registry/ModItems.java
@@ -1,6 +1,6 @@
 package com.NoComment1105.periodic.registry;
 
-import com.NoComment1105.periodic.Periodic;
+import com.NoComment1105.periodic.periodic;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
@@ -9,16 +9,14 @@ import net.minecraft.util.registry.Registry;
 public class ModItems {
 
     //items
-    public static final Item URANIUM = new Item(new Item.Settings().group(Periodic.ITEM_GROUP));
+    public static final Item URANIUM = new Item(new Item.Settings().group(periodic.ITEM_GROUP));
 
     //Block Items
-    public static final BlockItem URANIUM_BLOCK = new BlockItem(ModBlocks.URANIUM_BLOCK, new Item.Settings().group(Periodic.ITEM_GROUP));
-    public static final BlockItem URANIUM_ORE = new BlockItem(ModBlocks.URANIUM_ORE, new Item.Settings().group(Periodic.ITEM_GROUP));
+    public static final BlockItem URANIUM_BLOCK = new BlockItem(ModBlocks.URANIUM_BLOCK, new Item.Settings().group(periodic.ITEM_GROUP));
 
-    //registering the existence of the items note that blocks need an item form too
+    //registering the existance of the items note that blocks need an item form too
     public static void registerItems() {
-        Registry.register(Registry.ITEM, new Identifier(Periodic.MOD_ID, "uranium"), URANIUM);
-        Registry.register(Registry.ITEM, new Identifier(Periodic.MOD_ID, "uranium_block"),URANIUM_BLOCK);
-        Registry.register(Registry.ITEM, new Identifier(Periodic.MOD_ID, "uranium_ore"),URANIUM_ORE);
+        Registry.register(Registry.ITEM, new Identifier(periodic.MOD_ID, "uranium"), URANIUM);
+        Registry.register(Registry.ITEM, new Identifier(periodic.MOD_ID, "uranium_block"),URANIUM_BLOCK);
     }
 }
