e12ae739e2e5e7c54a090f362a4571f8063fa2a5
NoComment1105 <ryan.lillistone@gmail.com>
2021-06-02 04:00:39 +0100

    change: Refactor Oregen to use the API and not Mixins

--- src/main/java/com/NoComment1105/periodic/periodic.java
+++ src/main/java/com/NoComment1105/periodic/periodic.java
@@ -5,6 +5,8 @@ import com.nocomment1105.Periodic.items.RegisterArmour;
 import com.nocomment1105.Periodic.registry.ModBlocks;
 import com.nocomment1105.Periodic.registry.ModItems;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
@@ -16,10 +18,15 @@ import net.minecraft.structure.rule.BlockMatchRuleTest;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.BuiltinRegistries;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.gen.GenerationStep;
 import net.minecraft.world.gen.YOffset;
+import net.minecraft.world.gen.decorator.Decorator;
+import net.minecraft.world.gen.decorator.RangeDecoratorConfig;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
 import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.OreFeatureConfig;
+import net.minecraft.world.gen.heightprovider.UniformHeightProvider;
 
 import static com.nocomment1105.Periodic.registry.ModItems.*;
 import static com.nocomment1105.Periodic.registry.ModItems.ReinforcedUraniumAxeItem.REINFORCED_URANIUM_AXE;
@@ -38,16 +45,20 @@ public class periodic implements ModInitializer {
     //Setting ore generation characteristics
     public static final ConfiguredFeature<?, ?> ORE_URANIUM_ORE_OVERWORLD = Feature.ORE
             .configure(new OreFeatureConfig(OreFeatureConfig.Rules.BASE_STONE_OVERWORLD, ModBlocks.URANIUM_ORE.getDefaultState(),
-                    4)).uniformRange(YOffset.fixed(6), YOffset.fixed(36)).spreadHorizontally().repeat(10);
+                    4)).decorate(Decorator.RANGE.configure(new RangeDecoratorConfig(UniformHeightProvider.create
+                    (YOffset.fixed(6), YOffset.fixed(36))))).spreadHorizontally().repeat(10);
     public static final ConfiguredFeature<?, ?> ORE_ALUMINIUM_ORE_OVERWORLD = Feature.ORE
             .configure(new OreFeatureConfig(OreFeatureConfig.Rules.BASE_STONE_OVERWORLD, ModBlocks.ALUMINIUM_ORE.getDefaultState(),
-                    12)).uniformRange(YOffset.fixed(10), YOffset.fixed(60)).spreadHorizontally().repeat(13);
+                    12)).decorate(Decorator.RANGE.configure(new RangeDecoratorConfig(UniformHeightProvider.create
+                    (YOffset.fixed(10), YOffset.fixed(60))))).spreadHorizontally().repeat(13);
     public static final  ConfiguredFeature<?, ?> ORE_POTASSIUM_ORE_END = Feature.ORE
             .configure(new OreFeatureConfig(new BlockMatchRuleTest(Blocks.END_STONE), ModBlocks.POTASSIUM_ORE.getDefaultState(),
-                    5)).uniformRange(YOffset.fixed(10), YOffset.fixed(80)).spreadHorizontally().repeat(10);
+                    5)).decorate(Decorator.RANGE.configure(new RangeDecoratorConfig(UniformHeightProvider.create
+                    (YOffset.fixed(10), YOffset.fixed(80))))).spreadHorizontally().repeat(10);
     public static final ConfiguredFeature<?, ?> ORE_SILVER_ORE_NETHER = Feature.ORE
             .configure(new OreFeatureConfig(OreFeatureConfig.Rules.BASE_STONE_NETHER, ModBlocks.SILVER_ORE.getDefaultState(),
-                    6)).uniformRange(YOffset.fixed(60), YOffset.fixed(128)).spreadHorizontally().repeat(11);
+                    6)).decorate(Decorator.RANGE.configure(new RangeDecoratorConfig(UniformHeightProvider.create
+                    (YOffset.fixed(60), YOffset.fixed(128))))).spreadHorizontally().repeat(11);
     //fluids
     public static FlowableFluid STILL_MERCURY;
     public static FlowableFluid FLOWING_MERCURY;
@@ -60,9 +71,26 @@ public class periodic implements ModInitializer {
         ModBlocks.registerBlocks();
         RegisterArmour.register();
         //registering my ore generation
-        Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, new Identifier("periodic", "ore_uranium_ore_overworld"), ORE_URANIUM_ORE_OVERWORLD);
-        Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, new Identifier("periodic", "ore_aluminium_ore_overworld"), ORE_ALUMINIUM_ORE_OVERWORLD);
-        Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, new Identifier("periodic", "ore_potassium_ore_end"), ORE_POTASSIUM_ORE_END);
+        RegistryKey<ConfiguredFeature<?, ?>> oreUraniumOverworld = RegistryKey.of(Registry.CONFIGURED_FEATURE_KEY,
+                new Identifier("periodic", "ore_uranium_ore_overworld"));
+        Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, oreUraniumOverworld.getValue(), ORE_URANIUM_ORE_OVERWORLD);
+        BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES, oreUraniumOverworld);
+
+        RegistryKey<ConfiguredFeature<?, ?>> oreAluminiumOverworld = RegistryKey.of(Registry.CONFIGURED_FEATURE_KEY,
+                new Identifier("periodic", "ore_aluminium_ore_overworld"));
+        Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, oreAluminiumOverworld.getValue(), ORE_ALUMINIUM_ORE_OVERWORLD);
+        BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES, oreAluminiumOverworld);
+
+        RegistryKey<ConfiguredFeature<?, ?>> orePotassiumEnd = RegistryKey.of(Registry.CONFIGURED_FEATURE_KEY,
+                new Identifier("periodic", "ore_potassium_ore_end"));
+        Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, orePotassiumEnd.getValue(), ORE_POTASSIUM_ORE_END);
+        BiomeModifications.addFeature(BiomeSelectors.foundInTheEnd(), GenerationStep.Feature.UNDERGROUND_ORES, orePotassiumEnd);
+
+        RegistryKey<ConfiguredFeature<?, ?>> oreSilverNether = RegistryKey.of(Registry.CONFIGURED_FEATURE_KEY,
+                new Identifier("periodic", "ore_silver_ore_nether"));
+        Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, oreSilverNether.getValue(), ORE_SILVER_ORE_NETHER);
+        BiomeModifications.addFeature(BiomeSelectors.foundInTheNether(), GenerationStep.Feature.UNDERGROUND_ORES, oreSilverNether);
+
         // Registering Tools
         Registry.register(Registry.ITEM, new Identifier("periodic", "uranium_pickaxe"), URANIUM_PICKAXE);
         Registry.register(Registry.ITEM, new Identifier("periodic", "uranium_sword"), URANIUM_SWORD);
