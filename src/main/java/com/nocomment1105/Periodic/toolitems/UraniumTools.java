46aefdd5a89b574c7c72d1c02a23225e7cd44427
NoComment1105 <ryan.lillistone@gmail.com>
2021-06-20 06:13:31 +0100

    *Why was this file left behind*

---
+++ src/main/java/com/nocomment1105/Periodic/toolitems/UraniumTools.java
@@ -0,0 +1,33 @@
+package com.nocomment1105.Periodic.toolitems;
+
+import com.nocomment1105.Periodic.registry.ModItems;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.recipe.Ingredient;
+
+public class UraniumTools implements ToolMaterial {
+    public static final UraniumTools INSTANCE = new UraniumTools();
+    @Override
+    public int getDurability() {
+        return 650;
+    }
+    @Override
+    public float getMiningSpeedMultiplier() {
+        return 7.0F;
+    }
+    @Override
+    public float getAttackDamage() {
+        return 5.0F;
+    }
+    @Override
+    public int getMiningLevel() {
+        return 2;
+    }
+    @Override
+    public int getEnchantability() {
+        return 15;
+    }
+    @Override
+    public Ingredient getRepairIngredient() {
+        return Ingredient.ofItems(ModItems.URANIUM);
+    }
+}
